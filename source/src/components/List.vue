9975f9446ba7435e2bf4be838ec6abc32f4a0ad3
iuunhao <html.5@foxmail.com>
2017-07-13 00:43:38 +0800

    update

--- source/src/components/List.vue
+++ source/src/components/List.vue
@@ -11,7 +11,7 @@
 			<transitionGroup class="post" name="fade">
 				<li class="post__item" v-for="(item, index) in postListEd" key="item" >
 					<router-link class="post__link" :to="{name: 'detail', params: { url: item.url}}">
-						<p  class="post__date">{{ new Date(item.ctime).toLocaleDateString().replace(/(-|\/)/g, '') }}</p>
+						<p  class="post__date">{{new Date(item.ctime).format('yyyyMMdd')}}</p>
 						<h3 class="post__title">{{ item.name }}</h3>	
 						<p class="post__type">{{ item.type }}</p>
 						<em v-if="(Date.parse(new Date()) - item.ctime) < 86400000" class="new"></em>
@@ -23,6 +23,26 @@
 </template>
 <script>
 import FILES from '@/data/files';
+Date.prototype.format = function(fmt) { 
+	var o = { 
+		"M+" : this.getMonth()+1,                 //月份 
+		"d+" : this.getDate(),                    //日 
+		"h+" : this.getHours(),                   //小时 
+		"m+" : this.getMinutes(),                 //分 
+		"s+" : this.getSeconds(),                 //秒 
+		"q+" : Math.floor((this.getMonth()+3)/3), //季度 
+		"S"  : this.getMilliseconds()             //毫秒 
+	}; 
+	if(/(y+)/.test(fmt)) {
+			fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+	}
+	for(var k in o) {
+		if(new RegExp("("+ k +")").test(fmt)){
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+		}
+	}
+	return fmt; 
+}
 export default {
 	name: 'list',
 	props:[
